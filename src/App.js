75134ed7bfee0b8422bc4845ce8dd7f5a271b3a8
Jonathan Canela <jonathancanela365@gmail.com>
2020-05-27 10:25:36 -0700

    Finished First Part MVP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import "./App.css";
+import Title from './InfoContainer/Title'
+import Date from './InfoContainer/Date'
+import ImgContainer from './InfoContainer/ImgContainer'
 
 function App() {
+  const [data, setData] = useState([])
+  useEffect(() =>{
+    axios
+      .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then(res => {
+        setData(res.data)
+      })
+      .catch((error) => {console.log('Error!', error)})
+  },[])
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <Title data = {data}/>
+      <ImgContainer data = {data}/>
+      <Date data = {data}/>
     </div>
   );
 }
---
+++ src/InfoContainer/ImgContainer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ImgContainer(props){
+    return(
+        <img src = {props.data.url}/>
+    )
+}
+
+export default ImgContainer;
